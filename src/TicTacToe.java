f01c0c06d70f8f0f7d5ab829c94a0c5e7f236317
Pierce Trotman <ptrotman22@gmail.com>
2020-03-08 21:17:37 -0400

    Created Starting Variables

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,23 @@
+/*
+Create grid with Array
+Player chooses X or O
+Player 1 - X or O
+Player 2 - Computer
+
+ */
+
 public class TicTacToe {
-    public static void main(String[] args)
+    public static void main(String[] args){
+        String[] grid = new String[9];
+
+        for (int i = 0; i < grid.length; i++){
+            grid[i] = String.valueOf((i+1));
+        }
+
+        String player = "X";
+        String computer = "Y";
+
+
+
+    }
 }
